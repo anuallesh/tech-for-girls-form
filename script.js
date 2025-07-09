c35e1a14f3d99bbba8f2d9faf9126b9772a28a7b
anuallesh <anualleshwaram15@gmail.com>
2025-07-09 20:30:20 +0530

    new

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ form.addEventListener("submit", async (e) => {
   formData.append("screenshot", file);
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbxbmivz8Gt67JO85fm-O14_UF4h4edjI44MwyJwqrSvntEPsKW2XzN61Up_S4T7152w/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbyv6x4aB2uzyBGqP58eAlE0ipNuepX4D9LXirlVYtNdzjsjhRlxQqkkxhHuEkyHwkip/exec", {
       method: "POST",
       body: formData,
     });
